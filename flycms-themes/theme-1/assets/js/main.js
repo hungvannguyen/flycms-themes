b2c1cec869f7886aeadbc03c6254f51c1c8542bb
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-08-14 11:09:40 +0700

    update

--- flycms-themes/theme-1/assets/js/main.js
+++ flycms-themes/theme-1/assets/js/main.js
@@ -101,3 +101,21 @@ if (toTopButton) {
         });
     };
 }
+async function fetchPosts(endpoint, page = 1, limit = 16, ajax = 1) {
+    try {
+        const url = window.location.origin + endpoint + (endpoint.includes('?') ? '&' : '?') + 'page=' + page + '&limit=' + limit + '&ajax=' + ajax;
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.text();
+        const dataStr = data.replaceAll('\n', '').replaceAll('&quot;', '"');
+        const jsonData = JSON.parse(dataStr);
+        console.log(jsonData);
+        return jsonData;
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+        return { data: [] };
+    }
+}
+
+export { fetchPosts };
